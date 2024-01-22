fc15e26d4879bd3168633a890959528034631902
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-22 17:57:13 -0400

    fix for 2-print.py

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-
-"""Module providing a function printing python version."""
-print("\"Programming is like building a multilingual puzzle\n")
+print("\"Programming is like building a multilingual puzzle")
