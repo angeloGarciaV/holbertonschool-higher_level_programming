e7be4b0c4ea175e8c99e8b856d0888caef139931
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-24 22:18:48 -0400

    simplifying function by a lot

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    j = 1
-    if b < 0:
-        b = abs(b)
-        c = 1/a
-        for i in range(b):
-            j *= c
-        return round(j, 2)
-    else:
-        for i in range(b):
-            j *= a
-    return j
+    return a ** b
