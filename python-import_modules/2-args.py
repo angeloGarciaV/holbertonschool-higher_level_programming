f7857e83ef38a8e382194c2c3597ca4cf3820ee3
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-25 14:30:39 -0400

    Fix for 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,10 +3,16 @@
 if __name__ == "__main__":
     import sys
 
-    count = len(sys.argv) -1
+    count = len(sys.argv) - 1
     args = sys.argv
     a = 1
-    print("{} Arguments:".format(count))
+    if count == 0:
+        print("{} arguments.".format())
+        sys.exit()
+    if count == 1:
+        print("1 argument:\n1: {}".format(args[1]))
+        sys.exit()
+    print("{} arguments:".format(count))
     for i in args:
         if i == args[0]:
             continue
