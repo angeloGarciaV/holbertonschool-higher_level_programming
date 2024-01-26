b957e5a698408b93999d6cc9e50ceb101c1676a5
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-26 15:01:55 -0400

    Created 4-new_in_list.py

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list)-1:
+        return my_list
+    else:
+        new_list = []
+        for i in my_list:
+            new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
