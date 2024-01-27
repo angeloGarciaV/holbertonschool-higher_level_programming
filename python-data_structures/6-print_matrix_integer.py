6b59560451d9a500c4a16f4e6f955fd99aa360a1
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-26 20:14:10 -0400

    Created 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,15 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print(matrix[i][j], end=" ")
+            print(f"{matrix[i][j]}", end=" ")
         print()
-
-
-"""Write a function that prints a matrix of integers.
-
-Prototype: def print_matrix_integer(matrix=[[]]):
-Format: see example
-You are not allowed to import any module
-You can assume that the list only contains integers
-You are not allowed to cast integers into strings
-You have to use str.format() to print integers"""
