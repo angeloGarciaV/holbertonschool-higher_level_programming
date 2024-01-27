bd40224c164bf25b1acfcb936361a819133b551e
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-26 22:58:10 -0400

    Fix for 10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     if len(my_list) == 0:
         return None
     new_list = my_list.copy()
-    for i in range(len(my_list)-1):
+    for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             new_list[i] = True
         else:
