44649b5f37fe8a43bcbd5ff74373b6ac0545c047
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-31 17:36:52 -0400

    Fix for 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    r_str = roman_string
+    s = roman_string
     result = 0
     roman_nums = {"I": 1,
                   "V": 5,
@@ -9,10 +9,12 @@ def roman_to_int(roman_string):
                   "C": 100,
                   "D": 500,
                   "M": 1000}
-    if not isinstance(r_str, str) or r_str is None:
+    if not isinstance(s, str) or s is None:
         return result
-    if r_str == "IX":
-        return 9
-    for i in r_str:
-        result += roman_nums[i]
+
+    for i, num in enumerate(s):
+        if i < len(s) - 1 and roman_nums[s[i]] < roman_nums[s[i + 1]]:
+            result -= roman_nums[num]
+        else:
+            result += roman_nums[num]
     return result
