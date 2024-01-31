8746d09a1411979b9aa9a8f93fa574cd1fdc3868
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-31 10:22:02 -0400

    Fix for 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for i in range(len(new_list) - 1):
-        if new_list[i] == search:
+    for i, val in enumerate(new_list):
+        if val == search:
             new_list[i] = replace
     return new_list
-
-
-"""Write a function that replaces all occurrences of an element by another in a new list.
-
-Prototype: def search_replace(my_list, search, replace):
-my_list is the initial list
-search is the element to replace in the list
-replace is the new element
-You are not allowed to import any module"""
