3a7ce2b0ad64290342c930aa5f0cca71fe0a219e
Angelo Garcia <angelogarcia1029@gmail.com>
2024-01-31 10:45:26 -0400

    Pycodestyle fix

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,19 +2,10 @@
 def best_score(a_dictionary):
     max_key = None
     max_val = 0
-    if a_dictionary == None:
+    if a_dictionary is None:
         return a_dictionary
     for key in a_dictionary:
         if a_dictionary[key] > max_val:
             max_key = key
             max_val = a_dictionary[key]
     return max_key
-
-
-"""Write a function that returns a key with the biggest integer value.
-
-Prototype: def best_score(a_dictionary):
-You can assume that all values are only integers
-If no score found, return None
-You can assume all students have a different score
-You are not allowed to import any module"""
