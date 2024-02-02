980ad925546244678135b18426ea5d230b9b4205
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-02 01:09:31 -0400

    Created 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class Square with size attribute"""
+
+
+class Square:
+    """Class Square with size attribute"""
+
+    def __init__(self, size):
+        """Initializes size as private attribute for Square class"""
+        self.__size = size
