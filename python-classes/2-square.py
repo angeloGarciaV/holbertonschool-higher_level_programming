861a58abb35441ed6b5966ec624544dba7283fed
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-02 11:42:15 -0400

    Fix for 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,9 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes size as private attribute for Square class"""
-        try:
-            self.__size = size
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            __size = size
