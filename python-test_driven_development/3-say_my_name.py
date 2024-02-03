c474ebe20ab65d50b39fab4b9cbdf5b22097fa6c
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-02 23:48:02 -0400

    Created 3-say_my_name.py

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for say_my_name function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints My name is <first name> <last name>
+    Args:
+        first_name (str): first name
+        last_name (str, optional): last name. Defaults to "".
+    Raises:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
