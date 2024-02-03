937908ff3cf9f73e45edbe73409ae45f71c1b36d
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-02 20:57:01 -0400

    Fix for 0-add_integer documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     """Add two integers
+
+    Args:
+        a int: first integer
+        b (int, optional): seocnd integer. Defaults to 98.
+
+    Raises:
+        TypeError: a must be an integer
+        TypeError: b must be an integer
+
+    Returns:
+        int: sum of a and b
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
