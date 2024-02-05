e9fca43addf0cfe0b9be3da02514ae6026580c69
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-05 19:32:31 -0400

    Created 2-rectangle.py

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Rectangle Class"""
+class Rectangle:
+    """Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        """initialize the rectangle"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Returns width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle. Must be integer and >= 0"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle. Must be integer and >= 0"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        x = self.width
+        y = self.height
+        return x * y
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        x = self.width
+        y = self.height
+        p = 2 * (x + y)
+        if x == 0 or y == 0:
+            return 0
+        return p
+"""
+Instantiation with optional width and height: def __init__(self, width=0, height=0):
+Public instance method: def area(self): that returns the rectangle area
+Public instance method: def perimeter(self): that returns the rectangle perimeter:
+    if width or height is equal to 0, perimeter is equal to 0
+"""
