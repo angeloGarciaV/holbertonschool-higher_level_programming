229ae123749d37bcb0983d83872364abd271da03
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-08 19:21:42 -0400

    Requirement fix for 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,7 +3,11 @@
 
 
 class MyList(list):
-    """MyList class."""
+    """_summary_
+
+    Args:
+        list (list): list to be inherited from
+    """
 
     def print_sorted(self):
         """Prints the list, but sorted (ascending sort)."""
