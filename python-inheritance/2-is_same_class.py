049842e27011b854a72cc5abe13a31856bfda704
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-08 19:18:07 -0400

    Requirement fix for 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,5 +3,13 @@
 
 
 def is_same_class(obj, a_class):
-    """ checks if the object is exactly an instance of the specified class"""
-    return type(obj) == a_class
+    """Checks if obj is exactly an instance of a_class;
+
+    Args:
+        obj (class): object to be checked
+        a_class (class): class to check against
+
+    Returns:
+        bool: True if the object is exactly an instance of the specified class;
+    """
+    return type(obj) is a_class
