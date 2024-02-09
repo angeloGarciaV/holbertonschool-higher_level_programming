7a3597777c03e194bbdd054856c9448811385639
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-09 01:35:49 -0400

    Doc fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+""" Module for read_file method"""
+
+
 def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to "".
+    """
     with open(filename) as f:
         for line in f:
             print(line, end="")
