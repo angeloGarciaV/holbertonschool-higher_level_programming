d96e9a41faa0c5d9344c1233d5cc6be59f4ceaa4
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-09 05:22:32 -0400

    Documentation fix

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""class_to_json module"""
+
+
 def class_to_json(obj):
     """Returns the dictionary description with simple data structure
     (list, dictionary, string, integer and boolean) for JSON serialization
