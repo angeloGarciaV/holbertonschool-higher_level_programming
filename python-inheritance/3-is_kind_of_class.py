7033f7a010a2122cff949269f1e8f1df7b13d1b6
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-08 22:22:23 -0400

    Requirement Fix

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -11,6 +11,7 @@ def is_kind_of_class(obj, a_class):
         a_class (class): class to be checked against
 
     Returns:
-        bool: True if obj is an instance of a class or one that inherited from, a_class.
+        bool: True if obj is an instance of a class
+        or one that inherited from, a_class.
     """
     return isinstance(obj, a_class)
