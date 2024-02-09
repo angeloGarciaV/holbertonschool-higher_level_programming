a627cc8f9cbd85d6c8b90f45ea446717981b1b48
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-09 05:40:34 -0400

    pycodestyle fix

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -19,12 +19,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """Returns the dictionary description with simple data structure"""
-        if (type(attrs) == list and
+        if (type(attrs) is list and
                 all(type(itms) == str for itms in attrs)):
             return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
         return self.__dict__
-
-
-"""Public method def to_json(self, attrs=None): that retrieves a dictionary representation of a Student instance (same as 8-class_to_json.py):
-If attrs is a list of strings, only attribute names contained in this list must be retrieved.
-Otherwise, all attributes must be retrieved"""
