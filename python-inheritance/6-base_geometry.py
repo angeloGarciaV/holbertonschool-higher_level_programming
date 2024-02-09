4c2436475e58903c83790d54b0cc3b45ce3e103f
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-08 22:57:26 -0400

    created 6-base_geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" BaseGeometry module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Area method
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
