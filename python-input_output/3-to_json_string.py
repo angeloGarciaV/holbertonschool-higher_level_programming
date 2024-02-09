c800cd7d929806fc1fdc693ba2e88ecadeb7a4dd
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-09 02:42:55 -0400

    Created 3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def to_json_string(my_obj):
+    """Converts a python object to a JSON string
+
+    Args:
+        my_obj (any): Python object to convert
+
+    Returns:
+        str: JSON string
+    """
+    import json
+    return json.dumps(my_obj)
