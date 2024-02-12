26f33b2c4d8a337806045de015d5793378d20a4a
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-12 18:22:40 -0400

    Pycodestyle fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ class Base:
 
     def __init__(self, id=None):
         """Initializes a new instance of Base."""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
