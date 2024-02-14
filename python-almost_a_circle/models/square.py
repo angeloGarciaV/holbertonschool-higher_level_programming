d54d97a02dc12e9a44a1787a6318f89f80da94e0
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-13 22:41:03 -0400

    Docstring fix for Square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,6 +4,12 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Square class that inherits from Rectangle
+
+    Args:
+        Rectangle (class): Rectangle class
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes a new instance of Square.
 
