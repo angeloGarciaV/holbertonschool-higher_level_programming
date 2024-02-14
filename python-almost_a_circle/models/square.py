dc973721a515d258ab0bfffc6689ce08fab15358
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-13 22:53:05 -0400

    Docstring fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -41,5 +41,15 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        self.width = value
-        self.height = value
+        """ Setter for size
+
+        Args:
+            value (int): size of the square
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+            self.height = value
