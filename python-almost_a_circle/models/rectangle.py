d9f98bb4b6b46be4e919d0918ad18b2ef633fd6e
Angelo Garcia <angelogarcia1029@gmail.com>
2024-02-15 11:24:38 -0400

    fixing validation in rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,7 +79,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value <= 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be > 0")
         if value == 0:
             return ""
         else:
