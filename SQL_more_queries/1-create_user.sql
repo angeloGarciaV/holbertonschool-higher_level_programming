33b24525dfaab90574596bafe81ca2c5c7a795ed
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-15 20:58:30 -0400

    1-create_user.sql. Creates user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
 -- creates user user_0d_1 with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
