0eae1ac84cf287bb71470077cf83925971f0e7d5
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-15 22:24:39 -0400

    7-cities.sql. Creates database and table for cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+    UNIQUE(id)
 );
