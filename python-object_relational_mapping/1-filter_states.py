581d801a0d4015c941a78cb98368e2becbe8ae5a
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-20 14:08:15 -0400

    Fixed 1-filter_states.py. Formatting

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""List all states that starts with 'N"""
+"""List all states that starts with 'N' """
+
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost',user= sys.argv[1],
@@ -9,8 +11,7 @@ if __name__ == "__main__":
                         port = 3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
-                WHERE name LIKE BINARY 'N%'
-                ORDER BY id ASC""")
+                WHERE name LIKE BINARY 'N%' ORDER BY id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
