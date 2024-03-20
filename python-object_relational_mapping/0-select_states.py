f2ea7fadcdb462b55dce2ca988a0757f35501351
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-19 20:33:28 -0400

    initial commit. Added 0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         user= 'root',
+                         passwd='',
+                         db='hbtn_0e_0_usa')
+    cur = db.cursor()
+    cur.execute( "SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
