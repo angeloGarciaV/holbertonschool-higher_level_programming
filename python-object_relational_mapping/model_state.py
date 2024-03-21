fe17af7e38df2ca413eaf7cc3140d9193696efc6
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-21 12:46:49 -0400

    Added model_state.py. file that contains the class definition of a State

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""file that contains the class definition of a State"""
+from sys import argv
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column("id", Integer, primary_key=True,
+                autoincrement=True, unique=True)
+    name = Column("name", String(128), nullable=False)
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+engine = create_engine(
+    'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+    pool_pre_ping=True)
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
