5fce29455674658ea3db203534c26d91df0b3048
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-21 17:31:35 -0400

    fix for 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -4,8 +4,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', user='root',
-                         passwd='', db='hbtn_0e_4_usa',
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3],
                          port=3306)
     cur = db.cursor()
     cur.execute("""
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                 ON state_id = states.id
                 WHERE states.name LIKE BINARY %s
                 ORDER BY cities.id ASC;
-                """, {'state_name': argv[1]})
+                """, {'state_name': argv[4]})
     rows = cur.fetchall()
     for row in rows:
         print(row)
