34beb8be7877fc9abf68e5b3754a4882d8a57aa5
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-21 17:20:14 -0400

    fixing model_state.py.

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""File that contains the class definition of a State"""
-import sys
-from sqlalchemy import create_engine, Column, Integer, String
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-    """ Class State that inherits from Base """
-    __tablename__ = "state"
-    id = Column("id", Integer, primary_key=True,
-                autoincrement=True)
-    name = Column("name", String(128), nullable=False)
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
