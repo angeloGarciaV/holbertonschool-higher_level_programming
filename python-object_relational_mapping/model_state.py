8225135bd085cee43762315810944d14db025210
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-21 15:32:39 -0400

    Fix for model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """file that contains the class definition of a State"""
+
 from sys import argv
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -25,10 +25,7 @@ class State(Base):
         self.name = name
 
 
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
-    pool_pre_ping=True)
-Base.metadata.create_all(bind=engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
