e139847a808b5eaa0a6a1777fe2f3e8f3990eed9
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-20 21:37:39 -0400

    Added 3-my_safe_filter_states.py. Searches for the user input safely

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,11 +5,11 @@
 import MySQLdb
 from sys import argv
 
-srch = argv[3]
+srch = argv[4]
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=argv[1],
-                         passwd='', db=argv[2],
+                         passwd=argv[2], db=argv[3],
                          port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
