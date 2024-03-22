445b5c580dc5435eabf5df7a8f903ee7cdea6a3e
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-21 22:22:56 -0400

    format fix for 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,8 @@ from model_state import Base, State
 
 if __name__ == '__main__':
     engine = create_engine(
-        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}', pool_pre_ping=True)
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}',
+        pool_pre_ping=True)
 
 Session = sessionmaker(bind=engine)
 session = Session()
