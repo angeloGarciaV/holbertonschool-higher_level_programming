b15d97085ecdb23054e0b53a7cdb740d605c9285
Angelo Garcia <angelogarcia1029@gmail.com>
2024-03-22 21:27:51 -0400

    added 14-model_city_fetch_by_state.py.

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,9 +3,10 @@
 deletes all State objects with a name containing the letter a
 """
 from sys import argv
+from model_city import City
 from model_state import Base, State
 from sqlalchemy import create_engine, or_
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
@@ -14,14 +15,9 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(
-        or_(State.name.like('%a%'), State.name.like('%A%'))).all()
-    for state in states:
-        session.delete(state)
-    session.commit()
+    location = session.query(State, City).filter(
+        State.id == City.state_id).order_by(City.id,).all()
 
-    states = session.query(State).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
+    for state, city in location:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
